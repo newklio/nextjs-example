939e411fff089fcf6c7bb62f9ea22267c4de5606
deepak404found <deepakyadu404@gmail.com>
2022-09-06 12:17:53 +0530

    updated result login

--- pages/result/index.tsx
+++ pages/result/index.tsx
@@ -1,23 +1,59 @@
 import type { NextPage } from 'next'
 import styles from '../../styles/main.module.scss';
+import Grid from '@mui/material/Grid'
+import Paper from '@mui/material/Paper'
+import Button from '@mui/material/Button'
+import Divider from '@mui/material/Divider'
+import TextField from '@mui/material/TextField'
+import Box from '@mui/material/Box'
+import {BsGithub} from 'react-icons/bs'
+import { Helmet } from 'react-helmet'
+import Typography from '@mui/material/Typography';
 const Result: NextPage = () => {
   return  (
 		<>
-    <div className={styles.topBar}>
-			<h1>This is Result Page</h1>
-    </div>
-			<div className={styles.mainPage}>
-				<div className={styles.loginBox}>
-					<div>
-						<h3>Username</h3>
-						<input type="test" name="username" id="uname" />
-						<h3>Password</h3>
-						<input type="password" name="pass" id="pass" />
-						<br />
-						<button>Submit</button>
-					</div>
-				</div>
-			</div>
+			<Helmet>
+				<style>{'body { background-color: #222f3e; }'}</style>
+			</Helmet>
+			<Grid>
+				<Paper className={styles.loginContainer}>
+					<Box 
+						style={{
+							fontFamily: 'ubuntu',
+							fontSize: '90%',
+							fontWeight: '500',
+							opacity: '.92',
+						  }}
+						  sx={{
+							justifyContent: 'center',
+							display: 'flex',
+							m: 3,
+							gap: 1.5,
+							flexDirection: 'column',
+						  }}
+					>	
+						<Divider component="div" role="presentation">
+						{/* any elements nested inside the role="presentation" preserve their semantics. */}
+							<Typography variant="h4">Login</Typography>
+						</Divider>
+						<TextField 
+							placeholder='Pick username'
+							size='small'
+						/>
+						<TextField 
+							placeholder='Pick Password'
+							size='small'
+						/>
+						<Button
+							variant='contained'
+							color='success'
+						>
+							<BsGithub /> {' '}
+							login
+						</Button>
+					</Box>
+				</Paper>
+			</Grid>
 		</>
 
 	)
